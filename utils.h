1099fbfc7781306049dcd37394558c8fb2ede569
jaypee <jean-paul.saysana@epitech.eu>
2015-05-14 02:45:18 +0200

    win64

--- utils.h
+++ utils.h
@@ -2,7 +2,9 @@
 # define	UTILS_H_
 
 #if defined WIN32
-# define CLEAN_SCREEN "CLS"
+# define CLEAN_SCREEN "cls"
+#elif defined WIN64
+# define CLEAN_SCREEN "cls"
 #elif defined __linux
 # define CLEAN_SCREEN "clear"
 #endif
